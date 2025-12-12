5739bfd2aa5046a253d3fc0f8bd3fa77950c35ae
YutaNihei <niheiyuta1996@gmail.com>
2025-12-12 09:21:36 +0900

    1111

--- page-availability.php
+++ page-availability.php
@@ -16,6 +16,28 @@
                     客室一覧
                 </h2>
             </div>
+            <div class="reservation-grid">
+                <article class="reservation-card">
+                    <div class="reservation-card-head">
+                        <div>
+                            <p class="reservation-card-eyebrow">
+                                Premium Ocean View
+                            </p>
+                            <h3 class="reservation-card-title">
+                                プレミアムオーシャンビュー
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="reservation-price">
+                        <p class="reservation-price-label">
+                            1泊
+                        </p>
+                        <p class="reservation-price-value">
+                            15,000円
+                        </p>
+                    </div>
+                </article>
+            </div>
         </section>
     </main>
 <?php get_footer(); ?>
